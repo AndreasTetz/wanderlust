5379c38a780c15b9bb9120194b5d1d22f79cfa05
AndreasTetz <16765871+AndreasTetz@users.noreply.github.com>
2020-03-26 13:16:55 +0100

    updated main.js

--- public/main.js
+++ public/main.js
@@ -1,11 +1,11 @@
 // Foursquare API Info
-const clientId = '';
-const clientSecret = '';
-const url = '';
+const clientId = 'VV2Z2FXJ5HTW2AKTDCOPU5MCELCELV3VK2HYG2LNG45YQS3K';
+const clientSecret = 'QPKIZNHCZHMACK3ALZ3PRWCUTAVAVKQWAMOXA1IROHYR1WRP';
+const url = 'https://api.foursquare.com/v2/venues/explore?near=';
 
 // OpenWeather Info
-const openWeatherKey = '';
-const weatherUrl = '';
+const openWeatherKey = '9f3fcd752fb661e7982a6d557c66660d';
+const weatherUrl = 'https://api.openweathermap.org/data/2.5/weather';
 
 // Page Elements
 const $input = $('#city');
@@ -15,22 +15,38 @@ const $container = $('.container');
 const $venueDivs = [$("#venue1"), $("#venue2"), $("#venue3"), $("#venue4")];
 const $weatherDiv = $("#weather1");
 const weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-
-// Add AJAX functions here:
-const getVenues = () => {
-
+//API Querries
+const getVenues = async () => {
+  const city = $input.val();
+  const urlToFetch = `${url}${city}&limit=10&client_id=${clientId}&client_secret=${clientSecret}&v=20200310`;
+  try {
+    const response = await fetch(urlToFetch);
+    if (response.ok) {
+      const jsonResponse = await response.json();
+      const venues = jsonResponse.response.groups[0].items.map(parameter => parameter.venue);
+      return venues;
+    }
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-const getForecast = () => {
-
+const getForecast = async () => {
+  const urlToFetch = `${weatherUrl}?q=${$input.val()}&APPID=${openWeatherKey}`
+  try {
+    const response = await fetch(urlToFetch);
+    if (response.ok) {
+      const jsonResponse = await response.json();
+      return jsonResponse;
+    }
+  } catch (error) {
+    console.log(error);
+  }
 }
-
 
 // Render functions
 const renderVenues = (venues) => {
   $venueDivs.forEach(($venue, index) => {
-    // Add your code here:
-
     let venueContent = '';
     $venue.append(venueContent);
   });
@@ -38,8 +54,6 @@ const renderVenues = (venues) => {
 }
 
 const renderForecast = (day) => {
-  // Add your code here:
-  
 	let weatherContent = '';
   $weatherDiv.append(weatherContent);
 }
